34bece2380dbd4ed0f7f364f6ee2b746be435542
Sergio Arevalo <chejoarevalo@gmail.com>
2011-03-28 22:47:21 -0600

    commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfertaLaboral::Application.config.secret_token = '3d515ad766c5b7c0c7940bb53daf80f537dd9d3935c08af464cbe1f4e797e24ca8ca1b7a534fe6a4e4f969ce20203947e9bf4de45ce66e2a8bc9799cdbfd71c7'
